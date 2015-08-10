75f4b13eed9d93617e16a8188d64c7797119a73a
spidoche <spidoche@Nicolass-MacBook-Pro.local>
2015-08-11 08:23:17 +0900

    Convert tab to space

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
  * Author URI: http://spidoche.com
 
  * USAGE : replace prefix_oop_ , PREFIX_OOP_ and Prefix_oop_ to your unique plugin id
- 
+
  */
 
 
@@ -16,7 +16,7 @@
 defined( 'ABSPATH' ) or die( 'Nothing to see here.' );
 
 
-// DEFINE CONSTANTS 
+// DEFINE CONSTANTS
 define( 'PREFIX_OOP_VERSION', '0.1');
 define( 'PREFIX_OOP_PATH', plugin_dir_path(__FILE__) );
 define( 'PREFIX_OOP_URL', plugins_url( '', __FILE__) ); // use plugin_dir_url(__FILE__) to include the last slash
@@ -28,6 +28,5 @@ require_once(PREFIX_OOP_PATH.'/admin/options.php');
 
 // INIT CLASS
 new Prefix_oop_options;
-
 
 ?>
